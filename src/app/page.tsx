8b7b72482a20bb2a6df5f1d2d25d8ac544c5e1e9
joseLucas-code <skyleer05@gmail.com>
2025-05-18 09:02:34 -0300

    feat: register working properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import LoginForm from "@/components/auth/login_form";
 import RegisterForm from "@/components/auth/register_form";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
 
 export default function Home() {
+  const [tabs, setTabs] = useState("login");
   return (
     <div className="flex-1 bg-[url(/cat_bg.png)] bg-cover bg-center">
       <div className="flex h-full w-full items-center justify-center p-2 backdrop-blur-xl">
         <Tabs
-          defaultValue="login"
+          value={tabs}
+          onValueChange={setTabs}
           className="border-border w-full max-w-sm rounded-md border bg-zinc-950 p-2"
         >
           <TabsList className="w-full">
@@ -18,7 +23,7 @@ export default function Home() {
             <LoginForm />
           </TabsContent>
           <TabsContent value="register">
-            <RegisterForm />
+            <RegisterForm setTabs={setTabs} />
           </TabsContent>
         </Tabs>
       </div>
