0ee1e9a5b2923b0fdcd17245f7865783437e3a15
joseLucas-code <skyleer05@gmail.com>
2025-05-18 17:04:50 -0300

    fix: useRouter

--- src/app/(auth)/auth/oauth/page.tsx
+++ src/app/(auth)/auth/oauth/page.tsx
@@ -1,11 +1,5 @@
-"use client";
-
-import { useRouter } from "next/router";
 import React from "react";
 
 export default function OAuthPage() {
-  const router = useRouter();
-  const code = router.query.code as string;
-  console.log(code);
   return <div>OAuthPage</div>;
 }
