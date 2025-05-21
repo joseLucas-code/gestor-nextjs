bd3f99b0dd6f55b221aa38f190ab1a9465781291
joseLucas-code <skyleer05@gmail.com>
2025-05-21 15:21:27 -0300

    refactor: don't repeat your self

--- src/components/home/home_item.controller.ts
+++ src/components/home/home_item.controller.ts
@@ -4,20 +4,19 @@ export function useHomeItemController({
   price,
   discount_percentage,
 }: HomeItemControllerPriceProps) {
-  function formatedPrice() {
-    return Number(price).toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-    });
-  }
-
-  function priceWithDiscount() {
-    return (Number(price) * (Number(discount_percentage) / 100)).toLocaleString(
+  function formatedPrice(Internalprice?: string) {
+    return Number(Internalprice ? Internalprice : price).toLocaleString(
       "pt-br",
       {
         style: "currency",
         currency: "BRL",
       },
+    );
+  }
+
+  function priceWithDiscount() {
+    return formatedPrice(
+      (Number(price) * (Number(discount_percentage) / 100)).toString(),
     );
   }
 
