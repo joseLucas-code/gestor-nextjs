3d48ab5a466a87bd157be76a8dab0daeda94e754
joseLucas-code <skyleer05@gmail.com>
2025-05-21 11:33:28 -0300

    style: home items working

--- src/components/home/home_item.tsx
+++ src/components/home/home_item.tsx
@@ -1,11 +1,42 @@
-import React from "react";
-import { HomeServiceItemProps } from "./types";
 import Image from "next/image";
+import { HomeServiceItemProps } from "./types";
+import { ShoppingCart, StarIcon } from "lucide-react";
+import { Button } from "../ui/button";
 
 export default function HomeServiceItem({ service }: HomeServiceItemProps) {
   return (
-    <div className="relative aspect-video rounded-lg border">
-      <Image src={service.image ?? ""} alt="Imagem do serviço" fill />
-    </div>
+    <article className="relative flex aspect-square justify-center overflow-hidden rounded-lg border">
+      <Image
+        src={service.image ?? ""}
+        alt="Imagem do serviço"
+        width={410}
+        height={410}
+        className="h-full w-full object-cover"
+      />
+      <div className="absolute inset-x-0 bottom-0">
+        <div className="bg-popover-secondary/80 text-popover-secondary-foreground flex items-end p-2 backdrop-blur-lg">
+          {/* left column */}
+          <div className="flex flex-1 flex-col gap-1">
+            <h3 className="text-lg font-semibold">{service.name}</h3>
+
+            <div className="flex items-center gap-1 text-sm">
+              <span className="flex items-center gap-2">
+                <StarIcon className="fill-popover-secondary-foreground size-4" />
+                4,3 /
+              </span>
+              <span>200 vendas</span>
+            </div>
+
+            <span className="font-medium">R$ {service.price}</span>
+          </div>
+
+          {/* right column */}
+          <Button>
+            <ShoppingCart />
+            Carrinho
+          </Button>
+        </div>
+      </div>
+    </article>
   );
 }
