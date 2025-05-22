7fdf46524703f9ce1bc9e971d8bc597f38be98bf
joseLucas-code <skyleer05@gmail.com>
2025-05-22 15:19:35 -0300

    feat: devtools on user store

--- src/stores/userStore/userStore.ts
+++ src/stores/userStore/userStore.ts
@@ -1,18 +1,28 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import { UserActions, UserState } from "./types";
 import { userSchema } from "@/schemas/userSchema";
 
-export const useUserStore = create<UserActions & UserState>((set) => ({
-  isLogged: false,
-  user: null,
-  setUser: (user) =>
-    set(() => {
-      const parsedUser = userSchema.safeParse(user);
-      if (parsedUser.success) {
-        return { user: user, isLogged: true };
-      } else {
-        return { user: null, isLogged: false };
-      }
-    }),
-  logout: () => set({ user: null, isLogged: false }),
-}));
+export const useUserStore = create<UserActions & UserState>()(
+  devtools(
+    (set) => ({
+      isLogged: false,
+      user: null,
+      setUser: (user) =>
+        set(
+          () => {
+            const parsedUser = userSchema.safeParse(user);
+            if (parsedUser.success) {
+              return { user: user, isLogged: true };
+            } else {
+              console.error("Erro de validação", parsedUser.error.format());
+              return { user: null, isLogged: false };
+            }
+          },
+          undefined,
+          "setUser",
+        ), // <-- nome da action visível no DevTools
+      logout: () => set({ user: null, isLogged: false }, undefined, "logout"), // idem
+    }), // <-- nome da store visível no DevTools
+  ),
+);
