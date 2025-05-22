e3cb4e8705da39f4749d4f2d1237979508dcfdfb
joseLucas-code <skyleer05@gmail.com>
2025-05-22 14:33:09 -0300

    fix: nextjs Image warning with width and height

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -5,8 +5,14 @@ import OpeningHours from "../ui/OpeningHours/OpeningHours";
 
 export default function Banner({ banner, opening_hours }: BannerProps) {
   return (
-    <article className="relative flex max-h-72 w-full items-center justify-center overflow-hidden lg:rounded-md">
-      <Image src={banner ?? ""} alt="Banner" width={1280} height={320} />
+    <article className="relative flex min-h-72 w-full items-center justify-center overflow-hidden lg:rounded-md">
+      <Image
+        src={banner ?? ""}
+        alt="Banner"
+        fill
+        className="object-cover"
+        priority
+      />
       <div className="bg-popover-secondary/80 text-popover-secondary-foreground absolute inset-0 flex flex-col items-center justify-start gap-2 pt-10 lg:hidden">
         <h1 className="flex items-center gap-2 text-xl font-medium md:text-2xl">
           <Codesandbox />
